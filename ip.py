c684608b97a10b708d620270dcf38b829c557422
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-02-12 23:45:18 -0500

    Delete ip.py

--- ip.py
+++
@@ -1,8 +0,0 @@
-from socket import gethostbyname
-
-print("\033[1;33m*[Bienvenido a IP-OSINT: ]*")
-print("")
-target = input("\033[1;32mIngrese el host: \033[1;37m")
-targetIP = gethostbyname(target)
-print(f"\033[1;31m-Target IP ===>', \033[1;37m{targetIP}")
-
